ed07dcd6039f198d1fbc95b8cc0f5b30c19a5ab7
Mideks <mideksyt@gmail.com>
2024-01-30 18:44:19 +0500

    Add simple bot, that do random moves
    Replace magic player and empty field strings to constants
    
    Improve user input
    
    Add draw check
    
    Add win check

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import random
+
+X_SYMBOL = "X"
+O_SYMBOL = "O"
+EMPTY = " "
+
+
 def print_field(field):
     lines = []
     for row in field:
@@ -7,13 +14,20 @@ def print_field(field):
 
 def get_player_move():
     # todo: добавить обработку ошибок
-    row = int(input("Введите строку (0, 1 или 2): "))
-    col = int(input("Введите столбик (0, 1 или 2): "))
+    row = int(input("Введите строку (1, 2 или 3): ")) - 1
+    col = int(input("Введите столбик (1, 2 или 3): ")) - 1
+    return row, col
+
+
+def get_bot_move():
+    row = random.randint(1, 3) - 1
+    col = random.randint(1, 3) - 1
+    print(f"Бот сходил на клетку ({row}, {col}")
     return row, col
 
 
 def update_field(field, row, col, player_symbol):
-    if field[row][col] == " ":
+    if field[row][col] == EMPTY:
         field[row][col] = player_symbol
         return True
     else:
@@ -22,26 +36,44 @@ def update_field(field, row, col, player_symbol):
 
 
 def check_win(field, current_player):
+    # проверяем победу в одной из строчек или в одном из столбцов
+    for i in range(3):
+        if all(field[i][j] == current_player for j in range(3)) or \
+           all(field[j][i] == current_player for j in range(3)):
+            return True
+
+    # проверяем победу в диагоналях
+    if all(field[i][i] == current_player for i in range(3)) or \
+       all(field[i][2 - i] == current_player for i in range(3)):
+        return True
+
     return False
 
 
 def check_draw(field):
+    if all(all(field[i][j] != EMPTY for i in range(3)) for j in range(3)):
+        return True
+
     return False
 
 
 def tick_tac_toe():
     field = [
-        [" ", " ", " "],
-        [" ", " ", " "],
-        [" ", " ", " "]
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY]
     ]
-    current_player = "X"
+    current_player = X_SYMBOL
 
     while True:
         print_field(field)
         print(f"Ход игрока {current_player}")
 
-        row, col = get_player_move()
+        if current_player == X_SYMBOL:
+            row, col = get_player_move()
+        else:
+            row, col = get_bot_move()
+
         if update_field(field, row, col, current_player):
             if check_win(field, current_player):
                 print_field(field)
@@ -53,10 +85,10 @@ def tick_tac_toe():
                 print(f"Ничья!")
                 break
 
-            if current_player == "X":
-                current_player = "O"
+            if current_player == X_SYMBOL:
+                current_player = O_SYMBOL
             else:
-                current_player = "X"
+                current_player = X_SYMBOL
 
 
 tick_tac_toe()
